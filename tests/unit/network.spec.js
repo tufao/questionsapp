0280e37ea1f5e1ea3a9285ab700951f80911ce7a
Hugo Silveira <hugo.silveira@beeinsight.com>
2019-11-01 20:01:13 +0000

    Fix mock result

--- tests/unit/network.spec.js
+++ tests/unit/network.spec.js
@@ -14,10 +14,40 @@ describe('Networking', () => {
   it('Check online', async () => {
     mock.onGet(`${store.getters.serviceUrl}/health`).reply(200,
       {
-        'status': 'ok'
+        'status': 'OK'
       });
 
     await store.dispatch('checkHealth');
     expect(store.getters.isOnline).toBeTruthy();
+  });
+
+  it('Questions update', async () => {
+    mock.onGet(`${store.getters.serviceUrl}/questions`).reply(200,
+      [{
+        'id': 1,
+        'question': 'Favourite programming language?',
+        'image_url': 'https://dummyimage.com/600x400/000/fff.png&text=question+1+image+(600x400)',
+        'thumb_url': 'https://dummyimage.com/120x120/000/fff.png&text=question+1+image+(120x120)',
+        'published_at': '2015-08-05T08:40:51.620Z',
+        'choices': [
+          {
+            'choice': 'Swift',
+            'votes': 2048
+          }, {
+            'choice': 'Python',
+            'votes': 1024
+          }, {
+            'choice': 'Objective-C',
+            'votes': 512
+          }, {
+            'choice': 'Ruby',
+            'votes': 256
+          }
+        ]
+      }]);
+
+    await store.dispatch('fetchQuestions');
+    expect(store.getters.questions.length).toBe(1);
+    expect(store.getters.questions[0].id).toBe(1);
   });
 });
