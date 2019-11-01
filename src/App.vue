bb379e623f12efe60098cd201dcb579dd70872f7
Hugo Silveira <hugo.silveira@beeinsight.com>
2019-11-01 17:53:40 +0000

    Show online status on page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
+    <div class="server-online-status" :class="{'status-on': isOnline, 'status-off': !isOnline}">Server </div>
     <LoadingScreen msg="Loading..."/>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import LoadingScreen from './components/LoadingScreen.vue'
 
 export default {
   name: 'app',
   components: {
     LoadingScreen
+  },
+
+  mounted () {
+    this.$store.dispatch('checkHealth');
+  },
+
+  computed: {
+    ...mapGetters([
+      'isOnline'
+    ])
   }
+
 }
 </script>
 
@@ -23,5 +36,26 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.server-online-status {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+}
+
+.status-on {
+  color: green;
+}
+.status-on:after {
+    content: 'online';
+}
+
+.status-off {
+  color: red;
+}
+
+.status-off:after {
+  content: 'offline';
 }
 </style>
