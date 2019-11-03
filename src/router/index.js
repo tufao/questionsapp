9b4966b02d62562c4638e81ec29ea8c6d18ed40f
Hugo Silveira <www.tufao@gmail.com>
2019-11-03 10:24:23 +0000

    Split app into several pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/questions',
+    name: 'Questions',
+    component: () => import(/* webpackChunkName: "questions" */ '../views/Questions.vue')
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
 ]
 
 const router = new VueRouter({
