0cc536d0cd0c52bf19b5f150325fcf643c44ea76
Hugo Silveira <www.tufao@gmail.com>
2019-11-03 21:55:35 +0000

    Rename view component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import QuestionsScreen from '../views/QuestionsScreen.vue'
+import Questions from '../views/Questions.vue'
 
 Vue.use(VueRouter)
 
@@ -8,12 +8,12 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: QuestionsScreen
+    component: Questions
   },
   {
     path: '/questions',
     name: 'Questions',
-    component: QuestionsScreen
+    component: Questions
   },
   {
     path: '/about',
